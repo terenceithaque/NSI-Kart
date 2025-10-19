ac528ef8d10c3e4f83c8b723441b95267ba6236f
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-10-19 15:24:36 +0200

    Le kart peut se déplacer dans plusieurs directions

--- kart.py
+++ kart.py
@@ -5,33 +5,97 @@ import pygame
 class Kart(pygame.sprite.Sprite):
     "Classe représentant un kart en course."
 
-    def __init__(self, fenetre:pygame.Surface, image:str="assets/images/kart1.png", x:int=0, y:int=0, vitesse_max=50.0, acceleration=0.5) -> None:
+    def __init__(self, fenetre:pygame.Surface, image:str="assets/images/kart1.png", x:int=0, y:int=0, vitesse_max=50.0, acceleration=0.5, direction="haut") -> None:
         """Initialise le kart
         - fenetre : fenêtre de jeu dans laquelle le kart est affiché
         - image : chemin de l'image représentannt le kart
         - x : position x de départ du kart
         - y : position y de départ du kart
         - vitesse_max : vitesse maximale du kart
-        - acceleration : taux d'accélération du kart par secondes, par défaut 0.5"""
+        - acceleration : taux d'accélération du kart par secondes, par défaut 0.5
+        - direction : direction dans laquelle le kart se dirige, par défaut vers le haut"""
 
         # Initialisation des attributs
         self.fenetre = fenetre
         self.chemin_image = image
+        # Image originale du kart
         self.image = pygame.image.load(self.chemin_image)
         self.image = pygame.transform.scale(self.image, (100, 100))
+
+    
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
-        self.vitesse = 0.0
+        self.vitesse_haut = 0.0
+        self.vitesse_bas = 0.0
+        self.vitesse_gauche = 0.0
+        self.vitesse_droite = 0.0
         self.acceleration = acceleration
         self.vitesse_max = vitesse_max
 
+        self.direction = direction
+
+
+    def changer_direction(self, nouvelle_direction:str) -> None:
+        "Modifie la direction du kart"
+        self.direction = nouvelle_direction
+
+
+    def mettre_a_jour_rotation(self):
+        """Met à jour l'angle de rotation du kart."""
+        angles = {
+            "haut":0,
+            "droite":-90,
+            "bas":180,
+            "gauche":90
+        }
+
+        angle = angles.get(self.direction, 0)
+        self.image = pygame.transform.rotate(self.image, angle)
+
+        # Conserver le centre après rotation
+        centre = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = centre        
 
     def accelerer(self) -> None:
         """Accélère le kart si sa vitesse est inférieure à la vitesse maximale."""
-        if self.vitesse < self.vitesse_max:
-            self.vitesse += (self.acceleration * self.vitesse_max) 
+        if self.direction == "haut":
+            if self.vitesse_haut < self.vitesse_max:
+                self.vitesse_haut += (self.acceleration * self.vitesse_max)
+                print("Vitesse du kart (haut) :", self.vitesse_haut)
+
+        elif self.direction == "bas":
+            if self.vitesse_bas < self.vitesse_max:
+                self.vitesse_bas += (self.acceleration * self.vitesse_max)
+                print("Vitesse du kart (bas) :", self.vitesse_bas)
+
+        elif self.direction == "gauche":
+            if self.vitesse_gauche < self.vitesse_max:
+                self.vitesse_gauche += (self.acceleration * self.vitesse_max)
+                print("Vitesse du kart (gauche) :", self.vitesse_gauche)
+
+        elif self.direction == "droite":
+            if self.vitesse_droite < self.vitesse_max:
+                self.vitesse_droite += (self.acceleration * self.vitesse_max)
+                print("Vitesse du kart (droite) :", self.vitesse_droite)
+
+
+    def deplacer(self) -> None:
+        """Déplace le kart dans sa direction actuelle."""
+        if self.direction == "haut":
+            self.rect.y -= self.vitesse_haut
+
+        elif self.direction == "bas":
+            self.rect.y += self.vitesse_bas
+
+        elif self.direction == "gauche":
+            self.rect.x -= self.vitesse_gauche
+
+        elif self.direction == "droite":
+            self.rect.x += self.vitesse_droite                                                
 
 
     def afficher(self) -> None:
--- main.py
+++ main.py
@@ -11,15 +11,42 @@ fenetre = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("NSI-Kart")
 
 execution = True
-kart = Kart(fenetre, "assets/images/kart1.png", 50, 50, 80, 0.5)
+kart_joueur = Kart(fenetre, "assets/images/kart1.png", 120, 600, 10.0, 0.05, "haut")
+acceleration = pygame.USEREVENT + 1
+pygame.time.set_timer(acceleration, 500)
 
 # Boucle principale
 while execution:
+
+    fenetre.fill((0, 0, 0))
+
+    touches = pygame.key.get_pressed()
+
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             execution = False
 
-    kart.afficher()        
+        if evenement.type == acceleration: 
+            kart_joueur.accelerer()   
+
+    if touches[pygame.K_RIGHT]:
+        kart_joueur.changer_direction("droite")
+
+    if touches[pygame.K_LEFT]:
+        kart_joueur.changer_direction("gauche")
+
+    if touches[pygame.K_UP]:
+        kart_joueur.changer_direction("haut")
+
+    if touches[pygame.K_DOWN]:
+        kart_joueur.changer_direction("bas")
+
+
+    kart_joueur.mettre_a_jour_rotation()
+    kart_joueur.deplacer()                
+    
+    
+    kart_joueur.afficher()        
 
     # Mettre à jour l'affichage
     pygame.display.flip()        
