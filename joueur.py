80d3b1ac4d616d7d584a3ef8ac8e7920b8711c21
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-10-21 12:11:09 +0200

    Ajouté la classe Joueur

---
+++ joueur.py
@@ -0,0 +1,19 @@
+# Script du joueur
+import pygame
+from kart import *
+
+
+class Joueur:
+    """Une classe liant tous les éléments du joueur (position, kart, etc)."""
+    def __init__(self, fenetre:pygame.Surface, kart:Kart, position_depart:int=12):
+        """Initialise le joueur.
+        - fenetre : fenêtre où les différentes informations concernant le joueur sont affichées
+        - kart: le kart du joueur
+        - position_depart : position de départ du joueur"""
+
+        # Initialisation des attributs
+        self.fenetre = fenetre
+        self.kart = kart # Kart du joueur
+        self.position_depart = position_depart # Position de départ du joueur
+
+        self.position = self.position_depart # Position actuelle du joueur dans le classement
